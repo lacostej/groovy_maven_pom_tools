5e313d950d3e10ba1c8cd935160904ad0c8ff228
jerome.lacoste <jerome.lacoste@gmail.com>
2009-10-29 16:49:20 +0100

    Don't ask to commit when no changes

--- tools.sh
+++ tools.sh
@@ -8,6 +8,11 @@ export GROOVY_XML_TOOLS_HOME=$install_dir/groovy_xml_tools
 export CLASSPATH=$GROOVY_XML_TOOLS_HOME:$PATH
 
 function svncommit() {
+  changes=`svn diff | wc -l`
+  if [ $changes = 0 ]; then
+    echo "no changes for $1"
+    exit
+  fi
   svn diff | less
   PS3="Do you want to commit ?"
   select answer in y n
